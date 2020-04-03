30f67cb7d4ce4ef12f28be5e2f6141ed32ab5bcb
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2020-04-04 01:49:44 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -197,12 +197,12 @@ removeItem(item){
 
 
         <Switch>
-          <Route exact path="the-pefect-shop/" component={() => <MainPage items={this.state.items} />} />
-          <Route path="the-pefect-shop/cart" component={()=> <CartPage checkOut={this.checkout} itemRemover={this.removeItem} cart={this.state.cart} />}/>
-          <Route path="the-pefect-shop/item/:id" render={ props => <ItemPage {...props} addItem={this.addItem} allItems={this.state.items}/>}/>
-          <Route path="the-pefect-shop/thanks" component={ThankyouPage}/>
-          <Route path="the-pefect-shop/login" component={()=> <Login signin={this.signin}/>}/>
-          <Route path="the-pefect-shop/signup" component={()=> <Signup signin={this.signin}/>}/>
+          <Route exact path="the-perfect-shop/" component={() => <MainPage items={this.state.items} />} />
+          <Route path="the-perfect-shop/cart" component={()=> <CartPage checkOut={this.checkout} itemRemover={this.removeItem} cart={this.state.cart} />}/>
+          <Route path="the-perfect-shop/item/:id" render={ props => <ItemPage {...props} addItem={this.addItem} allItems={this.state.items}/>}/>
+          <Route path="the-perfect-shop/thanks" component={ThankyouPage}/>
+          <Route path="the-perfect-shop/login" component={()=> <Login signin={this.signin}/>}/>
+          <Route path="the-perfect-shop/signup" component={()=> <Signup signin={this.signin}/>}/>
 
         </Switch>
 
