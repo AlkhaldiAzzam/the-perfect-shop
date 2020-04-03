489216d21f090f8756c2d2d9c6884cacbf82a959
Azzam Alkhaldi <azzamalkhaldi74@gmail.com>
2020-04-03 03:26:20 +0300

    Updates

---
+++ precache-manifest.da2d8c03c62a7d3c2740ce93d7749989.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ed7f3733ecba06022d4856a8b27c1b1",
+    "url": "/the-perfect-shop/index.html"
+  },
+  {
+    "revision": "268fb041dac0e418c8c2",
+    "url": "/the-perfect-shop/static/css/2.ef1efacd.chunk.css"
+  },
+  {
+    "revision": "31975e5039e48b491ded",
+    "url": "/the-perfect-shop/static/css/main.ab3c6824.chunk.css"
+  },
+  {
+    "revision": "268fb041dac0e418c8c2",
+    "url": "/the-perfect-shop/static/js/2.7d01282f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/the-perfect-shop/static/js/2.7d01282f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31975e5039e48b491ded",
+    "url": "/the-perfect-shop/static/js/main.9c0e16a4.chunk.js"
+  },
+  {
+    "revision": "3f22a0e4f5016d6c8cd6",
+    "url": "/the-perfect-shop/static/js/runtime-main.a66eb506.js"
+  }
+]);
